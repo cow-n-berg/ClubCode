ea20835e62f1a0797e443a10bf778199cd3f58cf
Вячеслав Диконов <sdiconov@mail.ru>
2020-03-16 22:00:46 +0300

    Russian Translation update
    The previous version got out of sync with the forked source. Only the About dialog remained translated. Please, update the rpm package!

--- translations/harbour-clubcode-ru.ts
+++ translations/harbour-clubcode-ru.ts
@@ -1,28 +1,28 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1">
+<TS version="2.0">
 <context>
     <name>About</name>
     <message>
-        <location filename="../qml/pages/About.qml" line="20"/>
         <source>About</source>
         <translation>О программе</translation>
     </message>
     <message>
-        <location filename="../qml/pages/About.qml" line="56"/>
-        <source>Generate barcodes by entering a code.&lt;p&gt;                 &lt;b&gt;Supports:&lt;/b&gt; Code 128, Code 39, Code 93, UPC-A, UPC-E, EAN-8, EAN-13, QR-Code and KIX.</source>
-        <translation>Генерирует штрихкоды по введенному числу.&lt;p&gt; &lt;b&gt;Поддерживает:&lt;/b&gt; Code 128, Code 39, UPC-A, UPC-E, EAN-8 EAN-13 QR-Code and KIX.</translation>
+        <source>Generate barcodes by entering a code.&lt;p&gt;                 &lt;b&gt;Supports:&lt;/b&gt; Code 128, Code 39, EAN-8 and EAN-13.</source>
+        <translation>Генерирует штрихкоды по введенному числу.&lt;p&gt;                 &lt;b&gt;Поддерживает:&lt;/b&gt; Code 128, Code 39, EAN-8 and EAN-13.</translation>
     </message>
     <message>
-        <location filename="../qml/pages/About.qml" line="66"/>
         <source>Author</source>
         <translation>Автор</translation>
+    </message>
+    <message>
+        <source>Generate barcodes by entering a code.&lt;p&gt;                 &lt;b&gt;Supports:&lt;/b&gt; Code 128, Code 39, Code 93, UPC-A, UPC-E, EAN-8, EAN-13, QR-Code and KIX.</source>
+        <translation>Генерирует штрихкоды по введенному числу.&lt;p&gt;                 &lt;b&gt;Поддерживает:&lt;/b&gt; Code 128, Code 39, Code 93, UPC-A, UPC-E, EAN-8, EAN-13, QR-Code и KIX.</translation>
     </message>
 </context>
 <context>
     <name>AddNewCodePage</name>
     <message>
-        <location filename="../qml/pages/AddNewCodePage.qml" line="27"/>
         <source>Create code</source>
         <translation>Создать код</translation>
     </message>
@@ -30,135 +30,110 @@
 <context>
     <name>EditCodeTemplate</name>
     <message>
-        <location filename="../qml/EditCodeTemplate.qml" line="57"/>
         <source>Barcode type</source>
         <translation>Тип штрихкода</translation>
     </message>
     <message>
-        <location filename="../qml/EditCodeTemplate.qml" line="61"/>
         <source>Code 128</source>
         <translation>Code 128</translation>
     </message>
     <message>
-        <location filename="../qml/EditCodeTemplate.qml" line="64"/>
         <source>EAN 8</source>
         <translation>EAN 8</translation>
     </message>
     <message>
-        <location filename="../qml/EditCodeTemplate.qml" line="67"/>
         <source>EAN 13</source>
         <translation>EAN 13</translation>
     </message>
     <message>
-        <location filename="../qml/EditCodeTemplate.qml" line="70"/>
         <source>Code 39</source>
         <translation>Code 39</translation>
     </message>
     <message>
-        <location filename="../qml/EditCodeTemplate.qml" line="73"/>
-        <source>Code 93</source>
-        <translation type="unfinished">Code 93</translation>
-    </message>
-    <message>
-        <location filename="../qml/EditCodeTemplate.qml" line="76"/>
-        <source>UPC-E</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../qml/EditCodeTemplate.qml" line="79"/>
-        <source>UPC-A</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../qml/EditCodeTemplate.qml" line="82"/>
-        <source>QR-Code</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../qml/EditCodeTemplate.qml" line="85"/>
-        <source>KIX</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../qml/EditCodeTemplate.qml" line="97"/>
         <source>Name</source>
         <translation>Имя</translation>
     </message>
     <message>
-        <location filename="../qml/EditCodeTemplate.qml" line="119"/>
         <source>Description</source>
         <translation>Описание</translation>
     </message>
     <message>
-        <location filename="../qml/EditCodeTemplate.qml" line="142"/>
         <source>Code</source>
         <translation>Код</translation>
+    </message>
+    <message>
+        <source>Code 93</source>
+        <translation>Code 93</translation>
+    </message>
+    <message>
+        <source>UPC-E</source>
+        <translation>UPC-E</translation>
+    </message>
+    <message>
+        <source>UPC-A</source>
+        <translation>UPC-A</translation>
+    </message>
+    <message>
+        <source>QR-Code</source>
+        <translation>QR-Code</translation>
+    </message>
+    <message>
+        <source>KIX</source>
+        <translation>KIX</translation>
     </message>
 </context>
 <context>
     <name>HomePage</name>
     <message>
-        <location filename="../qml/pages/HomePage.qml" line="13"/>
-        <source>About</source>
-        <translation type="unfinished">О программе</translation>
-    </message>
-    <message>
-        <location filename="../qml/pages/HomePage.qml" line="17"/>
-        <source>Settings</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../qml/pages/HomePage.qml" line="21"/>
         <source>Add a new Code</source>
-        <translation type="unfinished"></translation>
+        <translation>Добавить новый код</translation>
     </message>
     <message>
-        <location filename="../qml/pages/HomePage.qml" line="31"/>
+        <source>About</source>
+        <translation>О программе</translation>
+    </message>
+    <message>
         <source>Barcodes</source>
-        <translation type="unfinished"></translation>
+        <translation>Штрихкоды</translation>
     </message>
     <message>
-        <location filename="../qml/pages/HomePage.qml" line="91"/>
         <source>Edit</source>
-        <translation type="unfinished"></translation>
+        <translation>Редактировать</translation>
     </message>
     <message>
-        <location filename="../qml/pages/HomePage.qml" line="98"/>
         <source>Remove</source>
-        <translation type="unfinished"></translation>
+        <translation>Удалить</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Настройка</translation>
     </message>
 </context>
 <context>
     <name>SettingPage</name>
     <message>
-        <location filename="../qml/pages/SettingPage.qml" line="29"/>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Настройка</translation>
     </message>
     <message>
-        <location filename="../qml/pages/SettingPage.qml" line="35"/>
         <source>Orientation</source>
-        <translation type="unfinished"></translation>
+        <translation>Ориентация</translation>
     </message>
     <message>
-        <location filename="../qml/pages/SettingPage.qml" line="36"/>
         <source>How the barcode should be displayed</source>
-        <translation type="unfinished"></translation>
+        <translation>Как следует показывать коды</translation>
     </message>
     <message>
-        <location filename="../qml/pages/SettingPage.qml" line="42"/>
         <source>Dynamic</source>
-        <translation type="unfinished"></translation>
+        <translation>Динамическая</translation>
     </message>
     <message>
-        <location filename="../qml/pages/SettingPage.qml" line="45"/>
         <source>Landscape</source>
-        <translation type="unfinished"></translation>
+        <translation>Пейзажная</translation>
     </message>
     <message>
-        <location filename="../qml/pages/SettingPage.qml" line="48"/>
         <source>Portrait</source>
-        <translation type="unfinished"></translation>
+        <translation>Портретная</translation>
     </message>
 </context>
 </TS>
